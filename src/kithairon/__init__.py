d2aec347e21cc7266176fe30b9181fa10643c3b9
Constantine Evans <const@costi.net>
2024-06-23 00:49:33 +0100

    Split

--- src/kithairon/__init__.py
+++ src/kithairon/__init__.py
@@ -1,10 +1,12 @@
 """A library for working with the Echo liquid handler."""
 
-
 from .labware import Labware, PlateInfo
 from .picklists import PickList
 from .surveys import SurveyData
 
+import importlib.util
+if importlib.util.find_spec("kithairon_extra"):
+    from kithairon_extra import *
 
 __all__ = [
     "SurveyData",
